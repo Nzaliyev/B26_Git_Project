d5f87e5c91b561311837e5596c3542912bd40f68
Nihat <n.zaliyev@gmail.com>
2022-03-26 12:56:20 -0700

    Created new package Added class to package

---
+++ src/day0/HelloWorld.java
@@ -0,0 +1,13 @@
+package day0;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    public static void greeting(){
+        System.out.println("HelloWorld");
+    }
+
+}
