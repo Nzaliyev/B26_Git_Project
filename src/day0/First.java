321003c344b6abc1371089f150700b77123c724c
Nihat <n.zaliyev@gmail.com>
2022-03-26 13:11:02 -0700

    First Class just got updated with cool code

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,7 +2,11 @@ package day0;
 
 public class First {
 
-    //TODO: Works on this tomorrow
-    // add some cool code
+    public static void main(String[] args) {
+        System.out.println("some cool code");
+    }
+    //extra line 1
+    //extra line 2
+    //extra line 3
 
 }
